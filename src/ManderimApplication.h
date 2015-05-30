13c91c4b9a8285537e760b7354059300f899cbf2
Tobias Stamm <manderby@manderby.com>
2015-05-30 16:48:29 +0200

    asdf

--- src/ManderimApplication.h
+++ src/ManderimApplication.h
@@ -1,6 +1,5 @@
 // Entwickelt von Tobias Stamm, Manderim GmbH, http://manderim.ch
 
-//#define MANDERIM_APPLICATION_REGISTRATION
 
 #import <Cocoa/Cocoa.h>
 #include "NASystem.h"
@@ -8,34 +7,13 @@
 
 
 
-typedef enum{
-  MANDERIM_METADATA_PRICE,            // (NAInt*)
-  MANDERIM_METADATA_COUNT
-} ManderimMetaData;
-
-
-#ifdef MANDERIM_APPLICATION_REGISTRATION
-  #define MANDERIM_REGISTRATION_PAID 0x01  // Assumes the user has paid
-  #define MANDERIM_REGISTRATION_OFFLINE_WARNING   0x02
-#endif
-
-
-
 @class ManderimAboutWindowController;
 
 
 @interface ManderimApplication : NSApplication <NSApplicationDelegate> {
-  IBOutlet ManderimAboutWindowController* aboutwindowcontroller;
-  void* metadata[MANDERIM_METADATA_COUNT];
-
-  #ifdef MANDERIM_APPLICATION_REGISTRATION
-    NAInt registrationstate;
-    NSMenuItem* registermenuitem;
-    NSThread* registrationthread;
-  #endif
+  ManderimAboutWindowController* aboutwindowcontroller;
 }
 
-- (void)setMetaData:(void*)newdata forIndex:(NSUInteger)index;
 - (IBAction)showAbout:(id)sender;
 
 + (NSCursor*)allocCursorFromBasename:(NSString*)basename pointX:(CGFloat)x pointY:(CGFloat)y;
--- src/ManderimApplication.m
+++ src/ManderimApplication.m
@@ -5,29 +5,13 @@
 #include "NADateTime.h"
 
 
-#ifdef MANDERIM_APPLICATION_REGISTRATION
-  @interface ManderimApplication (_Private)
-    - (void)showRegistration:(id)sender;
-    - (void)checkRegistration:(id)obj;
-  @end
-  #define MANDERIM_OFFLINE_USES_TILL_WARNING 3
-  #define MANDERIM_MAX_OFFLINE_USES 6
-#endif
-
-
 @implementation ManderimApplication
 
 
 - (id)init{
   self = [super init];
-  for(NAInt i=0; i<MANDERIM_METADATA_COUNT; i++){metadata[i] = nil;}
   [self setDelegate:self];
   return self;
-}
-
-
-- (void)setMetaData:(void*)newdata forIndex:(NSUInteger)index{
-  metadata[index] = newdata;
 }
 
 
@@ -37,25 +21,11 @@
   [aboutmenuitem setTarget:self];
   [aboutmenuitem setAction:@selector(showAbout:)];
   
-  #ifdef MANDERIM_APPLICATION_REGISTRATION
-    registermenuitem = [[NSMenuItem alloc] initWithTitle:@"Registration & Updates" action:@selector(showRegistration:) keyEquivalent:@""];
-    [registermenuitem setTarget:self];
-    [applicationmenu insertItem:registermenuitem atIndex:[applicationmenu indexOfItem:aboutmenuitem] + 1];
-
-    registrationstate = [[NSUserDefaults standardUserDefaults] integerForKey:@"RegistrationState"];
-    registrationthread = [[NSThread alloc] initWithTarget:self selector:@selector(checkRegistration:) object:nil];
-    [registrationthread start];
-  #endif
 }
 
 
 - (void)dealloc{
   [aboutwindowcontroller release];
-  #ifdef MANDERIM_APPLICATION_REGISTRATION
-    [registermenuitem release];
-    [registrationthread release];
-  #endif
-  free(metadata[MANDERIM_METADATA_PRICE]);
   [super dealloc];
 }
 
@@ -67,126 +37,6 @@
 }
 
 
-
-#ifdef MANDERIM_APPLICATION_REGISTRATION
-  - (IBAction)showRegistration:(id)sender{
-    printf("Here we are\n");
-  }
-
-
-  - (void)checkRegistration:(id)obj{
-    if(!metadata[MANDERIM_METADATA_PRICE]){return;}
-    if(*(NAInt*)(metadata[MANDERIM_METADATA_PRICE]) == 0){return;}
-    
-    NSAutoreleasePool* autoreleasepool = [[NSAutoreleasePool alloc] init];
-    NABool mustshowwindow = NA_FALSE;
-    NABool forceregister = NA_FALSE;
-    NADateTime now = naMakeDateTimeNow();
-
-    // First, get and correct the local settings if necessary
-    int64 fusn = [[NSUserDefaults standardUserDefaults] integerForKey:@"FirstUncertainSecondNumber"];
-    int64 lastokchecknum = [[NSUserDefaults standardUserDefaults] integerForKey:@"LastSuccessfulRegistrationCheck"];
-    int64 lastbadchecknum = [[NSUserDefaults standardUserDefaults] integerForKey:@"LastUnsuccessfulRegistrationCheck"];
-    int64 usessincelastsuccessfulcheck = [[NSUserDefaults standardUserDefaults] integerForKey:@"UsesSinceLastSuccessfulCheck"];
-
-    NADateTime lastokcheckdate;
-    if(lastokchecknum){
-      if(lastokchecknum <= now.sisec){
-        lastokcheckdate = naMakeDateTimeWithNALibSecondNumber(lastokchecknum);
-      }else{
-        mustshowwindow = NA_TRUE;
-        forceregister = NA_TRUE;
-        printf("You seem to have an invalid value in your settings. Let me check your registration...");
-        lastokcheckdate = now;
-      }
-    }else{
-      lastokcheckdate = now;
-    }
-    double okdiff = naGetDateTimeDiff(&now, &lastokcheckdate);
-
-    NADateTime lastbadcheckdate;
-    if(lastbadchecknum){
-      if(lastbadchecknum < now.sisec){
-        lastbadcheckdate = naMakeDateTimeWithNALibSecondNumber(lastbadchecknum);
-      }else{
-        mustshowwindow = NA_TRUE;
-        forceregister = NA_TRUE;
-        printf("You seem to have an invalid value in your settings. Let me check your registration...");
-        lastbadcheckdate = now;
-      }
-    }else{
-      lastbadcheckdate = now;
-    }
-    double baddiff = naGetDateTimeDiff(&now, &lastbadcheckdate);
-
-    if(usessincelastsuccessfulcheck < 0){
-      mustshowwindow = NA_TRUE;
-      forceregister = NA_TRUE;
-      printf("You seem to have an invalid value in your settings. Let me check your registration...");
-      usessincelastsuccessfulcheck = 0;
-    }
-    if(usessincelastsuccessfulcheck >= MANDERIM_MAX_OFFLINE_USES){
-      mustshowwindow = NA_TRUE;
-      forceregister = NA_TRUE;
-      printf("You can no longer use this application. Let me check your registration...");
-      usessincelastsuccessfulcheck = 0;
-    }
-    usessincelastsuccessfulcheck++;
-    
-    if(fusn){
-      NAInt correctconst = naGetLeapSecondCorrectionConstant(fusn);
-      switch(correctconst){
-      case NO_CORRECTION_NEEDED: break;
-      case INVALID_UNCERTAIN_SECOND_NUMBER:
-        mustshowwindow = NA_TRUE;
-        forceregister = NA_TRUE;
-        printf("You seem to have an invalid value in your settings. Let me check your registration...");
-        break;
-      case NEW_LIBRARY_IS_OLDER_THAN_BEFORE: break;
-        // The user has downgraded the application. No valid method possible.
-        mustshowwindow = NA_TRUE;
-        forceregister = NA_TRUE;
-        printf("You seem to have an invalid value in your settings. Let me check your registration...");
-      default:
-        naCorrectDateTimeForLeapSeconds(&lastokcheckdate, correctconst);
-        naCorrectDateTimeForLeapSeconds(&lastbadcheckdate, correctconst);
-        break;
-      }
-    }
-
-    if(mustshowwindow){
-//      [registrationwindow show];
-    }
-
-    NSURL* url;
-    url = [NSURL URLWithString:@"http://localhost"];
-    NSString* returnstring = [NSString stringWithContentsOfURL:url encoding:NSUTF8StringEncoding error:NULL];
-    returnstring = nil;
-    
-    if(returnstring){
-      usessincelastsuccessfulcheck = 0;
-      lastokcheckdate = naMakeDateTimeNow();
-    }else{
-      if(forceregister){
-        // If we force to register, there is no way around it.
-        printf("Registration required. Please connect to the Internet and restart this application.");
-      }else{
-        if(usessincelastsuccessfulcheck <= MANDERIM_OFFLINE_USES_TILL_WARNING){
-          // Just go along with it.
-        }else{
-          printf("You have not used this application while connected to the Internet lately.\nI must check your registration.\n\nYou can use this application %d more times without connecting to the Internet.", (MANDERIM_MAX_OFFLINE_USES - (int)usessincelastsuccessfulcheck) + 1);
-        }
-      }
-    }
-    
-    [[NSUserDefaults standardUserDefaults] setInteger:lastokcheckdate.sisec forKey:@"LastSuccessfulRegistrationCheck"];
-    [[NSUserDefaults standardUserDefaults] setInteger:lastbadcheckdate.sisec forKey:@"LastUnsuccessfulRegistrationCheck"];
-    [[NSUserDefaults standardUserDefaults] setInteger:naGetFirstUncertainSecondNumber() forKey:@"FirstUncertainSecondNumber"];
-    [[NSUserDefaults standardUserDefaults] setInteger:usessincelastsuccessfulcheck forKey:@"UsesSinceLastSuccessfulCheck"];
-
-    [autoreleasepool release];
-  }
-#endif
 
 
 + (NSCursor*)allocCursorFromBasename:(NSString*)basename pointX:(CGFloat)x pointY:(CGFloat)y{
