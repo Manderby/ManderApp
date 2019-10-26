5526f9d40487e5009e495be90b3aa7bbcb2a1e3d
Tobias Stamm <manderby@manderby.com>
2019-10-26 22:51:51 +0200

    Interim commit

--- src/ManderApp.c
+++ src/ManderApp.c
@@ -10,18 +10,22 @@ void mandInitManderApp(void){
   mandInitManderAppPrefs();
 }
 
+
+
 void mandCreateUI(void){
   mandCreateAboutController();
 }
 
+
+
 void mandAlertNewVersion(const NAUTF8Char* infoText){
-  NAString* curVersion = naNewBundleVersionString();
+  NAString* curVersion = naNewApplicationVersionString();
   NAString* lastOpenedVersion = naNewPreferencesString(ManderAppPrefs[LastOpenedVersion]);
   if(!naEqualStringToString(curVersion, lastOpenedVersion, NA_TRUE)){
     naSetPreferencesString(ManderAppPrefs[LastOpenedVersion], curVersion);
     
     const NAUTF8Char* formatstring = mandTranslate(MandNewVersionAlertTitle);
-    NAString* applicationname = naNewBundleApplicationName();
+    NAString* applicationname = naNewApplicationName();
     NAString* titleText = naNewStringWithFormat(formatstring, naGetStringUTF8Pointer(applicationname), naGetStringUTF8Pointer(curVersion));
     naPresentAlertBox(NA_ALERT_BOX_INFO, naGetStringUTF8Pointer(titleText), infoText);
     naDelete(applicationname);
