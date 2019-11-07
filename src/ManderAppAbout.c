509d440453a4ce7b6e428856f4e29a0ca4b9ba03
Tobias Stamm <manderby@manderby.com>
2019-11-07 19:22:14 +0100

    Interim Commit

--- src/ManderAppAbout.c
+++ src/ManderAppAbout.c
@@ -32,46 +32,46 @@ void mandCreateAboutController(void){
   
   NASpace* space = naGetWindowContentSpace(aboutWindow);
 
-  iconSpace = naNewImageSpace(naMakeRectS(106, 200, 128, 128));
-  NAString* bundleIconPath = naNewApplicationIconPath();
-  naSetImageSpacePath(iconSpace, naGetStringUTF8Pointer(bundleIconPath));
-  naDelete(bundleIconPath);
-  naAddSpaceChild(space, iconSpace);
+  //iconSpace = naNewImageSpace(naMakeSize(128, 128));
+  //NAString* bundleIconPath = naNewApplicationIconPath();
+  //naSetImageSpacePath(iconSpace, naGetStringUTF8Pointer(bundleIconPath));
+  //naDelete(bundleIconPath);
+  //naAddSpaceChild(space, iconSpace, naMakePos(106., 200.));
 
-  appName = naNewLabel(naGetStringUTF8Pointer(bundleApplicationName), naMakeRectS(20, 162, 300, 22));
+  appName = naNewLabel(naGetStringUTF8Pointer(bundleApplicationName), naMakeSize(300, 22));
   naSetLabelFontKind(appName, NA_FONT_KIND_TITLE);
   naSetLabelTextAlignment(appName, NA_TEXT_ALIGNMENT_CENTER);
-  naAddSpaceChild(space, appName);
+  naAddSpaceChild(space, appName, naMakePos(20., 162.));
 
-  NAString* bundleVersionString = naNewApplicationVersionString();
-  NAString* bundleBuildString = naNewApplicationBuildString();
-  const NAUTF8Char* aboutVersionFormatString = mandTranslate(MandVersionBuild);
-  NAString* aboutVersionString = naNewStringWithFormat(aboutVersionFormatString, naGetStringUTF8Pointer(bundleVersionString), naGetStringUTF8Pointer(bundleBuildString));
-  appVersion = naNewLabel(naGetStringUTF8Pointer(aboutVersionString), naMakeRectS(20, 140, 300, 22));
-  naDelete(aboutVersionString);
-  naDelete(bundleVersionString);
-  naDelete(bundleBuildString);
-  naSetLabelTextAlignment(appVersion, NA_TEXT_ALIGNMENT_CENTER);
-  naAddSpaceChild(space, appVersion);
+  //NAString* bundleVersionString = naNewApplicationVersionString();
+  //NAString* bundleBuildString = naNewApplicationBuildString();
+  //const NAUTF8Char* aboutVersionFormatString = mandTranslate(MandVersionBuild);
+  //NAString* aboutVersionString = naNewStringWithFormat(aboutVersionFormatString, naGetStringUTF8Pointer(bundleVersionString), naGetStringUTF8Pointer(bundleBuildString));
+  //appVersion = naNewLabel(naGetStringUTF8Pointer(aboutVersionString), naMakeSize(300, 22));
+  //naDelete(aboutVersionString);
+  //naDelete(bundleVersionString);
+  //naDelete(bundleBuildString);
+  //naSetLabelTextAlignment(appVersion, NA_TEXT_ALIGNMENT_CENTER);
+  //naAddSpaceChild(space, appVersion, naMakePos(20., 140.));
 
-  appDesc = naNewLabel("", naMakeRectS(20, 74, 300, 66));
+  appDesc = naNewLabel("", naMakeSize(300, 66));
   naSetLabelTextAlignment(appDesc, NA_TEXT_ALIGNMENT_CENTER);
-  naAddSpaceChild(space, appDesc);
+  naAddSpaceChild(space, appDesc, naMakePos(20., 74.));
 
-  helpLink = naNewLabel(mandTranslate(MandOnlineHelp), naMakeRectS(20, 65, 300, 22));
+  helpLink = naNewLabel(mandTranslate(MandOnlineHelp), naMakeSize(300, 22));
   naSetLabelTextAlignment(helpLink, NA_TEXT_ALIGNMENT_CENTER);
   naSetLabelLink(helpLink, "http://manderc.com");
-  naAddSpaceChild(space, helpLink);
+  naAddSpaceChild(space, helpLink, naMakePos(20., 65.));
 
-//  manderCSpace = naNewImageSpace(naMakeRectS(120, 70, 100, 21));
+//  manderCSpace = naNewImageSpace(naMakeSize(100, 21));
 //  NAString* bundleResourcePath = naNewApplicationResourcePath("manderc", "png");
 //  naSetImageSpacePath(manderCSpace, naGetStringUTF8Pointer(bundleResourcePath));
 //  naDelete(bundleResourcePath);
-//  naAddSpaceChild(space, manderCSpace);
+//  naAddSpaceChild(space, manderCSpace, naMakePos(120., 70.));
 
-  doneButton = naNewPushButton(mandTranslate(MandDone), naMakeRectS(130, 20, 80, 24));
+  doneButton = naNewPushButton(mandTranslate(MandDone), naMakeSize(80, 24));
   naAddUIReaction(doneButton, NA_UI_COMMAND_PRESSED, pressAboutDone, NA_NULL);
-  naAddSpaceChild(space, doneButton);
+  naAddSpaceChild(space, doneButton, naMakePos(130., 20.));
 
   naSetButtonSubmit(doneButton, pressAboutDone, NA_NULL);
   naSetButtonAbort(doneButton, pressAboutDone, NA_NULL);
